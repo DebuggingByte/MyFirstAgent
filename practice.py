6f4eb04add4f83a77ede918ae577d34898852915
Mikael Jahangir <ghostsnightmaref@gmail.com>
2025-07-03 14:42:20 -0700

    Made practice.py and finished create_llm_message.py

--- practice.py
+++ practice.py
@@ -2,9 +2,11 @@ import streamlit as st
 from langchain_core.messages import SystemMessage, BaseMessage
 
 def create_llm_msg(system_prompt: str, sessionHistory: list[BaseMessage]) -> list[BaseMessage]:
-    resp = [] 
-    resp.append(SystemMessage(content="system_prompt"))
+    resp = []
+    resp.append(SystemMessage(content=system_prompt))
     resp.extend(sessionHistory)
     return resp
-    
+
+
+
 
