80db88dca8dcfa536169586c9163e219f1c64d51
Mikael Jahangir <ghostsnightmaref@gmail.com>
2025-07-03 15:30:57 -0700

    Finished math Agent prompt. And deleted prompt_store.py.

--- math_agent.py
+++ math_agent.py
@@ -6,7 +6,18 @@ from create_llm_message import create_llm_msg
 class MathAgent:
     def __init__(self, model):
         self.model = model
-        self.system_prompt = "You are a helpful assistant that can answer questions about math."
+        self.system_prompt = """
+        You are a math teacher that can answer questions about math. 
+        When someone asks you a question, follow these steps:
+        1. You should first think about the question and then help them solve it. 
+        2. You should also be able to explain your answer in a way that is easy to understand. 
+        3. Make sure you never ever give the answer directly, and help them solve it on their own.
+        4. Make sure that you answer in detail and the simplest way possible. If you are not sure about the answer, you should say so.
+        5. If the user asks you to solve a problem, you should first think about the problem and then help them solve it.
+        6. If the user asks you to explain a concept, you should first think about the concept and then help them understand it.
+        7. If the user asks you to do a calculation, you should first think about the calculation and then help them do it.
+       
+        """
         self.sessionHistory = []
 
     def get_response(self, user_input: str):
